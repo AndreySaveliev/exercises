22122af61f166ae9b5949161baccb6b4b26dc5d9
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-05-31 13:12:54 +0300

    feat/created detail component

---
+++ src/components/Detail.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Typography, Stack, Button } from '@mui/material';
+import { exercise } from '../utils/types';
+
+import BodyPartImg from './../assets/icons/body-part.png';
+import TargetImg from './../assets/icons/target.png';
+import Equipment from './../assets/icons/equipment.png';
+
+const Detail = ({ exerciseDitail }: { exerciseDitail: exercise }) => {
+  const extraDetail = [
+    {
+      icon: BodyPartImg,
+      name: exerciseDitail.bodyPart
+    },
+    {
+      icon: TargetImg,
+      name: exerciseDitail.target
+    },
+    {
+      icon: Equipment,
+      name: exerciseDitail.equipment
+    }
+  ];
+
+  return (
+    <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }}>
+      <img
+        src={exerciseDitail.gifUrl}
+        alt={exerciseDitail.name}
+        loading="lazy"
+        className="detail-image"
+      />
+      <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
+        <Typography variant="h3" textTransform='capitalize'>{exerciseDitail.name}</Typography>
+        <Typography variant="h6">
+          Exercises keep you strong.{' '}
+          <span style={{ textTransform: 'capitalize' }}>{exerciseDitail.name}</span>
+          is one of the best <br /> exercises to target your {exerciseDitail.target}. It will help
+          you improve your <br /> mood and gain energy.
+        </Typography>
+        {extraDetail.map((detail) => (
+          <Stack key={detail.name} direction="row" gap="24px" alignItems="center">
+            <Button
+              sx={{ background: '#fff2db', borderRadius: '50%', width: '100px', height: '100px' }}
+            >
+              <img src={detail.icon} alt={detail.name} style={{ width: '50xp', height: '50px' }} />
+            </Button>
+            <Typography variant="h5" textTransform='capitalize'>{detail.name}</Typography>
+          </Stack>
+        ))}
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Detail;
